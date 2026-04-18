f41c2c1be120cdb3b2d3a76482b46a691e0e62e2
AnimeshSSE <animeshj.sse@gmail.com>
2026-04-18 08:44:35 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    "@prisma/client",
+    "@prisma/adapter-libsql",
+    "@libsql/client",
+    "libsql",
+  ],
 };
 
 export default nextConfig;
